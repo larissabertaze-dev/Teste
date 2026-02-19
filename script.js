e80c32c31f634835c94b6ee7538f094ddbd71aaf
Larissa Bertaze <larissabertaze@gmail.com>
2026-02-19 15:56:39 -0300

    Teste

--- script.js
+++ script.js
@@ -8,18 +8,3 @@ questions.forEach(question => {
             answer.style.display === "block" ? "none" : "block";
     });
 });
-
-<script>
-
-const btns = document.querySelectorAll('.acc-btn');
-
-btns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    const content = btn.nextElementSibling;
-
-    content.style.display =
-      content.style.display === 'block' ? 'none' : 'block';
-  });
-});
-
-</script>
